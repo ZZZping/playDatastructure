e9a65b27b610305810101a26a68fd17201d590e2
ZZZping <475377765@qq.com>
2020-03-06 00:32:54 +0800

    add linkedlist methods

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -24,4 +24,72 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+    private Node head;
+    int size;
+
+    public LinkedList(){
+        head = null;
+        size = 0;
+    }
+
+    /**
+     * 获取链表中元素个数
+     */
+    public int getSize(){
+        return size;
+    }
+
+    /**
+     * 判断链表是否为空
+     */
+    public boolean isEmpty9(){
+        return size == 0;
+    }
+
+    /**
+     * 在链表头添加元素e
+     */
+    public void addFirst(E e){
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+
+        head = new Node(e, head);
+        size++;
+    }
+
+    /**
+     * 在链表中某一位置插入元素
+     */
+    public void add(int index, E e){
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        }
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i ++) {
+                prev = prev.next;
+            }
+
+//            Node node = new Node(e);
+//            node.next = prev.next;
+//            prev.next = node;
+
+            prev.next = new Node(e, prev.next);
+            size++;
+
+        }
+    }
+
+    /**
+     * 在链表末尾添加元素
+     */
+    public void addLast(E e){
+//        Node node = new Node(e);
+//        node.next = null;
+        add(size, e);
+    }
 }
