f97cd6cb139f36255ef52a65dc5357b12947d2dd
ZZZping <475377765@qq.com>
2020-03-13 23:09:52 +0800

    2

--- leetcode_203/src/Solution2.java
+++ leetcode_203/src/Solution2.java
@@ -1,3 +1,17 @@
 public class Solution2 {
-
+    public ListNode removeElements(ListNode head, int val){
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                //前置结点prev只想的元素为需要删除的元素
+                ListNode delNode = prev.next;
+                delNode.next.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return dummyHead;
+    }
 }
