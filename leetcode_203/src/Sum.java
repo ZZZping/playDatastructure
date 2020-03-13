355aad7085f074d5c0956fc215c470b09f60af4d
ZZZping <475377765@qq.com>
2020-03-13 23:18:47 +0800

    2

--- leetcode_203/src/Sum.java
+++ leetcode_203/src/Sum.java
@@ -1,2 +1,22 @@
 public class Sum {
+    public static int sum(int[] arr) {
+        return sum(arr,0);
+    }
+
+    /**
+     * @param arr 目标数组
+     * @param l 开始左边界
+     * @return 递归调用，return arr[l
+ + sum(arr, l+1)     */
+    private static int sum(int[] arr, int l){
+        if (l == arr.length) {
+            return 0;
+        }
+        return arr[l] +sum(arr, l + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(sum(nums));
+    }
 }
