28852e504ef628e53a796b7a631942ac65040c7e
ZZZping <475377765@qq.com>
2020-03-16 00:24:46 +0800

    1

--- leetcode_203/src/Solution3.java
+++ leetcode_203/src/Solution3.java
@@ -1,10 +1,29 @@
 public class Solution3 {
-    public ListNode removeElements(ListNode head, int val){
+    public ListNode removeElements(ListNode head, int val, int depth){
+        String depthString = generateDepthString(depth);
+        System.out.print(depthString);
+        System.out.println("Call: remove " + val + " in " + head);
         if (head == null) {
-            return null;
+            System.out.print(depthString);
+            System.out.println("Return: " + head);
+            return head;
         }
-        head.next = removeElements(head.next, val);
-        return head.val == val ? head.next : head;
+        ListNode res = removeElements(head.next, val, depth + 1);
+        System.out.print(depthString);
+        System.out.println("After remove " + val + ": " + res);
+        ListNode ret;
+        if (head.val == val) {
+            ret = res;
+        } else {
+            head.next = res;
+            ret = head;
+        }
+        System.out.print(depthString);
+        System.out.println("Return ret");
+        return ret;
+
+//        head.next = removeElements(head.next, val, depth + 1);
+//        return head.val == val ? head.next : head;
 //        ListNode res = removeElements(head.next, val);
 //        if (head.val == val) {
 //            return res;
@@ -13,6 +32,23 @@ public class Solution3 {
 //            return head;
 //        }
 
+    }
+
+    public String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i ++) {
+            res.append("--");
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution3()).removeElements(head,6, 0);
+        System.out.println(res);
     }
 
 
