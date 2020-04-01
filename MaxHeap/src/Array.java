046540bc14e90957344a5317a63015260d29b320
ZZZping <475377765@qq.com>
2020-04-01 17:59:51 +0800

    heapify & replay

--- MaxHeap/src/Array.java
+++ MaxHeap/src/Array.java
@@ -19,6 +19,14 @@ public class Array<E>{
         this(10);
     }
 
+    public Array(E[] arr){
+        data = (E[])new Object[arr.length];
+        for (int i = 0; i < arr.length; i ++) {
+            data[i] = arr[i];
+        }
+        size = arr.length;
+    }
+
     /**
      * 获取数组中元素的个数
      * @return 返回数组中元素个数
