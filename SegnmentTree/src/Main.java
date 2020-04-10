221deb5490e4d51d4a5f3bc6f92ca7d0f1abbd34
ZZZping <475377765@qq.com>
2020-04-10 17:13:10 +0800

    leetcode_0303

--- SegnmentTree/src/Main.java
+++ SegnmentTree/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Integer[] nums = {-2, 0, 3, -5, 2, -1};
-        SegnmentTree<Integer> segnmentTree = new SegnmentTree<>(nums, (a, b) -> a + b);
+        SegnmentTree<Integer> segnmentTree = new SegnmentTree<>(nums, Integer::sum);
         System.out.println(segnmentTree);
         //-2 + 0 + 3 = 1
         System.out.println(segnmentTree.query(0, 2));
