d456ec3759b694036e6acd8d1cb2134c3b144b56
ZZZping <475377765@qq.com>
2020-04-10 16:35:41 +0800

    query function

--- SegnmentTree/src/SegnmentTree.java
+++ SegnmentTree/src/SegnmentTree.java
@@ -54,6 +54,47 @@ public class SegnmentTree<E> {
         return 2 * index + 2;
     }
 
+    /**
+     * 查询[queryL, queryR]的值
+     * @param queryL 左边界
+     * @param queryR 右边界
+     * @return 返回查询值
+     */
+    public E query(int queryL, int queryR){
+        if (queryL < 0 || queryL >= data.length || queryR < 0 || queryR >= data.length || queryL > queryR) {
+            throw new IllegalArgumentException("Index is illegal");
+        }
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    /**
+     * 在以treeID为根的线段树中[l,r]的范围里，搜索区间[queryL,queryR]的值
+     * @param treeIndex 根节点
+     * @param l 线段树左边界
+     * @param r 线段树右边界
+     * @param queryL 查询左边界
+     * @param queryR 查询右边界
+     * @return 返回查询结果
+     */
+    private E query(int treeIndex, int l, int r, int queryL, int queryR){
+        if (l == queryL && r == queryR) {
+            return tree[treeIndex];
+        }
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        if (queryL >= mid + 1) {
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+        } else if (queryR <= mid) {
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+        }
+
+        E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex,mid + 1, r, mid + 1, queryR);
+        return merger.merger(leftResult, rightResult);
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
