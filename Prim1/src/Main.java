7430e9afe6f6615ae1e4c33d6ce72a4d2a14de62
ZZZping <475377765@qq.com>
2020-04-18 11:26:44 +0800

    1

--- Prim1/src/Main.java
+++ Prim1/src/Main.java
@@ -2,12 +2,17 @@ import edu.princeton.cs.algs4.In;
 
 public class Main {
     public static void main(String[] args) {
-        In in = new In("tinyEWG.txt");
+        double startTime = System.nanoTime();
+        In in = new In("edge.txt");
         EdgeWeightedGraph G = new EdgeWeightedGraph(in);
         PrimMST mst = new PrimMST(G);
+        double endTime = System.nanoTime();
+        double time = (endTime - startTime) / 1000000000.0;
+        System.out.println("time is:" + time + " s");
         for (Edge e : mst.edges()) {
             System.out.println(e);
         }
         System.out.printf("%.5f\n", mst.weight());
+
     }
 }
