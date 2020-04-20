4abc659cca9089df9189ed9f45534a44ab4b482f
ZZZping <475377765@qq.com>
2020-04-20 23:37:13 +0800

    add

--- Kruskal&Prim/src/Main.java
+++ Kruskal&Prim/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         double startTime = System.nanoTime();
-        In in = new In("edge.txt");
+        In in = new In("large.txt");
         EdgeWeightedGraph G = new EdgeWeightedGraph(in);
         KruskalMST kruskalMST = new KruskalMST(G);
         double endTime = System.nanoTime();
--- Kruskal_Algorithm/src/Main.java
+++ Kruskal_Algorithm/src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args)  {
         double startTime = System.nanoTime();
-        In in = new In("edge.txt");
+        In in = new In("test2.txt");
         EdgeWeightedGraph G = new EdgeWeightedGraph(in);
         KruskalMST mst = new KruskalMST(G);
         double endTime = System.nanoTime();
         double time = (endTime - startTime) / 1000000000.0;
         System.out.println("Kruskal time is:" + time + " s");
-        for (Edge e : mst.edges()) {
-            System.out.println(e);
-        }
-        System.out.printf("%.5f\n", mst.weight());
+//        for (Edge e : mst.edges()) {
+//            System.out.println(e);
+//        }
+//        System.out.printf("%.5f\n", mst.weight());
     }
 }
