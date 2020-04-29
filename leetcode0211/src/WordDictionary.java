6036e2632f1aba26667d3ab8a4ce9b1727c7f51e
ZZZping <475377765@qq.com>
2020-04-29 16:55:06 +0800

    leetcode211

---
+++ leetcode0211/src/WordDictionary.java
@@ -0,0 +1,71 @@
+import java.util.TreeMap;
+
+public class WordDictionary {
+
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new Node();
+    }
+
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i ++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        cur.isWord = true;
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return match(root, word, 0);
+    }
+
+    /**
+     * 搜索函数
+     * @param node 以node为根节点
+     * @param word 搜索word
+     * @param index 以index处的字符搜索
+     * @return 返回是否找到
+     */
+    private boolean match(Node node, String word, int index) {
+        if (index == word.length()) {
+            return node.isWord;
+        }
+        char c = word.charAt(index);
+        if (c != '.') {
+            if (node.next.get(c) == null) {
+                return false;
+            }
+            return match(node.next.get(c), word, index + 1);
+        } else {
+            for (char nextChar: node.next.keySet()) {
+                if (match(node.next.get(nextChar), word, index + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
