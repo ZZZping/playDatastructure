5ba3e81ef7ec166603cc551641fb171ad5d251a0
ZZZping <475377765@qq.com>
2020-05-06 22:02:23 +0800

    AVL树

---
+++ AVLTree/src/AVLTree.java
@@ -0,0 +1,212 @@
+import java.util.ArrayList;
+
+public class AVLTree<K extends Comparable<K>, V> implements Map<K, V> {
+    private class Node {
+        public K key;
+        public V value;
+        public Node left, right;
+        public int height;
+
+        /**
+         * 节点函数
+         *
+         * @param key   map中的key值
+         * @param value key对应的value
+         */
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+            height = 1;
+        }
+    }
+    private Node root;
+    private int size;
+
+    public AVLTree(){
+        root = null;
+        size = 0;
+    }
+
+    @Override
+    public int getSize(){
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    /**
+     * 获得节点node的高度
+     * @param node 节点
+     * @return 返回节点高度
+     */
+    private int getHeight(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    /**
+     * 获得节点的平衡因子
+     * @param node 节点
+     * @return 返回平衡因子
+     */
+    private int getBalanceFactor(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    @Override
+    public void add(K key, V value){
+        root = add(root, key, value);
+    }
+
+    private Node add(Node node, K key, V value) {
+        if (node == null) {
+            size ++;
+            return new Node(key, value);
+        }
+        if (key.compareTo(node.key) < 0) {
+            node.left = add(node.left, key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = add(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        //计算高度
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+
+        //获得平衡因子
+        int balacneFctor = getBalanceFactor(node);
+        if (Math.abs(balacneFctor) > 1) {
+            System.out.println("unbalance : " + balacneFctor);
+        }
+        return node;
+    }
+
+    /**
+     * 返回以node为根节点的BST中，key所在的节点
+     * @param node 以node为节点
+     * @param key 寻找key节点
+     * @return 返回找到的节点
+     */
+    private Node getNode(Node node, K key){
+        if (node ==null) {
+            return null;
+        }
+        if (key.compareTo(node.key) == 0) {
+            return node;
+        } else if (key.compareTo(node.key) < 0) {
+            return getNode(node.left, key);
+        } else {
+            return getNode(node.right, key);
+        }
+    }
+
+    @Override
+    public boolean contains(K key){
+        return getNode(root, key) != null;
+    }
+
+    @Override
+    public V get(K key){
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+    @Override
+    public void set(K key, V newValue){
+        Node node = getNode(root, key);
+        if (node == null) {
+            throw new IllegalArgumentException(key + " doesn't exist!");
+        }
+        node.value = newValue;
+    }
+
+    private Node mininum(Node node){
+        if (node.left == null) {
+            return node;
+        }
+        return mininum(node.left);
+    }
+
+    private Node removeMin(Node node){
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size --;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    @Override
+    public V remove(K key){
+        Node node = getNode(root, key);
+        if (node != null) {
+            root = remove(root, key);
+            return node.value;
+        }
+        return null;
+    }
+
+    private Node remove(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+            return node;
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+            return node;
+        } else {
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return node;
+            }
+            Node successor = mininum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+            node.left = node.right = null;
+            return successor;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Pride and prejudice");
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt",words)) {
+            System.out.println("Total words: " + words.size());
+            AVLTree<String ,Integer> map = new AVLTree<>();
+            for (String word: words) {
+                if (map.contains(word)) {
+                    map.set(word, map.get(word) + 1);
+                } else {
+                    map.add(word, 1);
+                }
+
+            }
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of Pride: " + map.get("pride"));
+            System.out.println("Frequency of Prejudice: " + map.get("prejudice"));
+        }
+    }
+
+}
