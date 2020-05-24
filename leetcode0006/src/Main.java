13ef46dac692e7b44b7c001152963e08cb5c4a9e
ZZZping <475377765@qq.com>
2020-05-24 22:22:51 +0800

    leetcode0004

---
+++ leetcode0006/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] A = {1, 3};
+        int[] B = {2};
+        System.out.println(solution.findMedianSortedArrays(A, B));
+    }
+}
